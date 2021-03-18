30da6b041d9aafe55d2bf8b8370754bc770c9fb0
b0ho <ybg4828@gmail.com>
2021-03-19 02:39:59 +0900

    template

---
+++ back/zipgap_back/src/main/java/com/zipgap/ZipgapBackApplication.java
@@ -0,0 +1,13 @@
+package com.zipgap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipgapBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipgapBackApplication.class, args);
+	}
+
+}
