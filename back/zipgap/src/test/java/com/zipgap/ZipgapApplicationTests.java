f1573be4f19217dab9b9c1070f7d6311c6cb3b78
b0ho <ybg4828@gmail.com>
2021-03-19 03:19:22 +0900

    back

---
+++ back/zipgap/src/test/java/com/zipgap/ZipgapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipgap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipgapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
