62eeeaca6a64c67bb85cfbbc84fb55631ac51e06
b0ho <ybg4828@gmail.com>
2021-04-04 21:50:35 +0900

    jsx 적용, 폴더구조 변경

---
+++ front_react/src/Component/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from './SearchBar';
+import "./Style.css";
+import GetList from "./List/GetList";
+
+function Home() {
+  return (
+    <div>
+      <span>이건 홈 화면</span>
+      <div>
+        <SearchBar />
+      </div>
+      <GetList />
+      <span>이건 끝</span>
+    </div>
+  );
+}
+
+export default Home;
