4eee55be671249ea1e08367ef428d328875dda4d
b0ho <ybg4828@gmail.com>
2021-04-04 17:48:41 +0900

    axios test

--- front_react/src/Component/Home.js
+++ front_react/src/Component/Home.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import './Style.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
+import "./Style.css";
 
+const Home = () => {
+  const LIST_API_BASE_URL = "http://localhost:8080/lists";
 
-function Home() {
-    return (
-        <div>
-            <span>이건 홈 화면</span>
-            <div>
-                <SearchBar />
-            </div>
-        </div>
-    );
-}
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(LIST_API_BASE_URL).then((result) => setData(result.data));
+  }, []);
+
+  return (
+    <div>
+      <span>이건 홈 화면</span>
+      <div>
+        <SearchBar />
+      </div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.result}>
+            {item.result}: {item.result}
+          </li>
+        ))}
+      </ul>
+      <span>이건 끝</span>
+    </div>
+  );
+};
 
 export default Home;
