ecb87b220c4a8af77da685acc0b6778c1ce3890d
b0ho <ybg4828@gmail.com>
2021-04-04 21:25:57 +0900

    front-back-db 연결

--- front_react/src/Component/Home.js
+++ front_react/src/Component/Home.js
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React from "react";
 import SearchBar from "./SearchBar";
 import "./Style.css";
+import GetList from "./GetList";
 
 function Home() {
-  const LIST_API_BASE_URL = "http://localhost:8080/lists/20210101";
-
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  async function fetchUrl() {
-    const response = await fetch(LIST_API_BASE_URL);
-    const json = await response.json();
-  }
-
-  useEffect(() => {
-    axios
-      .get(LIST_API_BASE_URL)
-      .then((result) => setData(result.data))
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
-
   return (
     <div>
       <span>이건 홈 화면</span>
       <div>
         <SearchBar />
       </div>
-      {setData.apartment_name}:{data.apartment_name}
-      {setData}:<span>이건 끝</span>
+      <GetList />
+      <span>이건 끝</span>
     </div>
   );
 }
