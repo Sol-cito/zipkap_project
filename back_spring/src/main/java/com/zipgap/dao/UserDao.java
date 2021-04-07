8f906d7e8c2a46bc266ba0f84cd4f6f72f5558c4
dataenggu <dsolkim1030@gmail.com>
2021-04-07 22:57:00 +0900

    Create Registration flow

---
+++ back_spring/src/main/java/com/zipgap/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.zipgap.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+}
---
+++ back_spring/src/main/java/com/zipgap/service/memberService/IUserService.java
@@ -0,0 +1,7 @@
+package com.zipgap.service.memberService;
+
+import com.zipgap.vo.userVO.RegistrationVO;
+
+public interface IUserService {
+    void userRegistration(RegistrationVO registrationVO);
+}
