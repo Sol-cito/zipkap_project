df8fbe3dcfc2814b3c4fdac814be638328986a12
dataenggu <dsolkim1030@gmail.com>
2021-04-13 23:08:31 +0900

    finish registration validation

--- front_react/src/Component/Registration.jsx
+++ front_react/src/Component/Registration.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 // import { useForm } from 'react-hook-form'; // form 검증을 도와주는 라이브러리
 import axios from 'axios';
 import { checkEmailValidity, checkNicknameValidity, checkPasswordValidity } from './RegistrationRegExp';
@@ -14,10 +14,12 @@ function Registration() {
 
     /* 회원가입 유효성 검증 ('이름'은 유효성검증 안함) */
     const [isValidEmail, setEmailValidity] = useState(false);
+    const [isNameFillt, setNameValidity] = useState(false);
     const [isValidNickname, setNicknameValidity] = useState(false);
     const [isValidPassword, setPasswordValidity] = useState(false);
     const [isValidPasswordCheck, setPasswordCheckValidity] = useState(false);
-
+    
+    /* 비밀번호 및 비밀번호 확인 관련 메시지를 dynamic하게 보여주기 위해 설정 */
     useEffect(() => {
         comparePasswordAndPasswordCheck();
     }, [password, passwordCheck]) // 배열에 담긴 것들이 effected 될 때만 useEffect가 작동한다.
@@ -38,13 +40,17 @@ function Registration() {
             case "email":
                 if (checkEmailValidity(e.target.value)) {
                     setEmailValidity(true);
-                    setEmail(e.target.value);
                 } else {
                     setEmailValidity(false);
-                    setEmail("");
                 }
+                setEmail(e.target.value);
                 break;
             case "name":
+                if (e.target.value.length != 0) {
+                    setNameValidity(true);
+                } else {
+                    setNameValidity(false);
+                }
                 setName(e.target.value);
                 break;
             case "nickName":
@@ -69,9 +75,27 @@ function Registration() {
         }
     }
 
-    /* 회원가입의 모든 boolean값이 true인지 검증 */
+    /* 최종 유효성 검증 후 focus를 줄 대상의 reference (각 input에 ref로 달려있다) */
+    const emailRef = useRef(null);
+    const nameRef = useRef(null);
+    const nickNameRef = useRef(null);
+    const passwordRef = useRef(null);
+    const passwordCheckRef = useRef(null);
+
+    /* 회원가입의 모든 boolean값이 true인지 최종 유효성 검증, false면 focue를 준다 */
     const finalValidationCheck = () => {
-        return isValidEmail && isValidNickname && isValidPassword && isValidPasswordCheck;
+        if (!isValidEmail) {
+            return emailRef.current.focus();
+        } else if (!isNameFillt) {
+            return nameRef.current.focus();
+        } else if (!isValidNickname) {
+            return nickNameRef.current.focus();
+        } else if (!isValidPassword) {
+            return passwordRef.current.focus();
+        } else if (!isValidPasswordCheck) {
+            return passwordCheckRef.current.focus();
+        }
+        return true;
     }
 
 
@@ -79,7 +103,6 @@ function Registration() {
     const submitClick = (e) => {
         e.preventDefault();
         if (!finalValidationCheck()) {
-            alert("검증 안됨");
             return;
         }
 
@@ -119,48 +142,57 @@ function Registration() {
     return (
         <div className="registration_div">
             <div>
-                <span>회원가입</span>
+                <h1>회원가입</h1>
             </div>
             <form className="registration_form" onSubmit={submitClick}>
-                <input className="registration_input" name="email" placeholder="이메일 주소" onChange={handleFromChange} />
+                <input className="registration_input" name="email" placeholder="이메일 주소" ref={emailRef} onChange={handleFromChange} />
                 {email !== null ? (isValidEmail ? (
                     <div>
                         <span>사용가능한 Email 입니다</span>
                     </div>
                 ) : (
                     <div>
-                        <span className='inValidForm'>잘못된 Email 양식입니다</span>
+                        <span className='inValidForm'>* 잘못된 Email 양식입니다</span>
                     </div>
                 )) : null}
-                <input id="name" className="registration_input" name="name" placeholder="성명" onChange={handleFromChange} />
-                <input className="registration_input" name="nickName" placeholder="닉네임" onChange={handleFromChange} />
+                <input id="name" className="registration_input" name="name" placeholder="성명" ref={nameRef} onChange={handleFromChange} />
+                {name !== null ? (isNameFillt ? (
+                    <div>
+                        <span>멋진 이름이네요!</span>
+                    </div>
+                ) : (
+                    <div>
+                        <span className='inValidForm'> * 이름을 입력해주세요</span>
+                    </div>
+                )) : null}
+                <input className="registration_input" name="nickName" placeholder="닉네임" ref={nickNameRef} onChange={handleFromChange} />
                 {nickName !== null ? (isValidNickname ? (
                     <div>
                         <span>사용가능한 닉네임입니다</span>
                     </div>
                 ) : (
                     <div>
-                        <span className='inValidForm'>닉네임은 1자 이상 8자 이하로 입력하셔야 합니다</span>
+                        <span className='inValidForm'>* 닉네임은 1자 이상 8자 이하로 입력하셔야 합니다</span>
                     </div>
                 )) : null}
-                <input className="registration_input" name="password" type="password" placeholder="비밀번호" onChange={handleFromChange} />
+                <input className="registration_input" name="password" type="password" placeholder="비밀번호" ref={passwordRef} onChange={handleFromChange} />
                 {password !== null ? (isValidPassword ? (
                     <div>
                         <span>사용가능한 비밀번호입니다</span>
                     </div>
                 ) : (
                     <div>
-                        <span className='inValidForm'> 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요</span>
+                        <span className='inValidForm'> * 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요</span>
                     </div>
                 )) : null}
-                <input className="registration_input" name="passwordCheck" type="password" placeholder="비밀번호 확인" onChange={handleFromChange} />
+                <input className="registration_input" name="passwordCheck" type="password" placeholder="비밀번호 확인" ref={passwordCheckRef} onChange={handleFromChange} />
                 {passwordCheck !== null ? (isValidPasswordCheck ? (
                     <div>
                         <span>비밀번호가 일치합니다</span>
                     </div>
                 ) : (
                     <div>
-                        <span className='inValidForm'>비밀번호가 일치하지 않습니다</span>
+                        <span className='inValidForm'>* 비밀번호가 일치하지 않습니다</span>
                     </div>
                 )) : null}
                 <div>
