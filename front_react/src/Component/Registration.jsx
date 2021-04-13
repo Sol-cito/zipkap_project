0bc7852d1b67c571795330033816f48231f1efbe
dataenggu <dsolkim1030@gmail.com>
2021-04-13 15:42:44 +0900

    edit registration validation check logic

--- front_react/src/Component/Registration.jsx
+++ front_react/src/Component/Registration.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import { useForm } from 'react-hook-form'; // form 검증을 도와주는 라이브러리
 import axios from 'axios';
 import { checkEmailValidity, checkNicknameValidity, checkPasswordValidity } from './RegistrationRegExp';
-import './Registration.css'
 
 
 function Registration() {
+    /* 회원가입 초기값 -1 로 세팅 */
     const [email, setEmail] = useState(-1);
     const [name, setName] = useState(-1);
     const [nickName, setNickname] = useState(-1);
@@ -14,14 +14,22 @@ function Registration() {
 
     /* 회원가입 유효성 검증 */
     const [isValidEmail, setEmailValidity] = useState(false);
+    const [isNameFillt, setNameValidity] = useState(false);
     const [isValidNickname, setNicknameValidity] = useState(false);
     const [isValidPassword, setPasswordValidity] = useState(false);
     const [isValidPasswordCheck, setPasswordCheckValidity] = useState(false);
 
-    /* 비밀번호 확인 유효성 검증 */
-    const handleCheckPassword = (e) => {
-        password === e.target.value ? setPasswordCheckValidity(true) : setPasswordCheckValidity(false);
-        setPasswordCheck(e.target.value);
+    useEffect(() => {
+        comparePasswordAndPasswordCheck();
+    }, [password, passwordCheck]) // 배열에 담긴 것들이 effected 될 때만 useEffect가 작동한다.
+
+    /* 
+     * 비밀번호 확인 유효성 검증 함수
+     * useEffect로 password, passwordCheck가 바뀔 때 두 변수를 비교하여 boolean값을 set한다.
+     */
+    const comparePasswordAndPasswordCheck = () => {
+        password === passwordCheck ? setPasswordCheckValidity(true) :
+            setPasswordCheckValidity(false);
     }
 
     /* form input 이 변경될 때마다 hook을 set하는 리스너 함수 */
@@ -37,6 +45,11 @@ function Registration() {
                 }
                 break;
             case "name":
+                if (e.target.value === "") {
+                    setNameValidity(false);
+                } else {
+                    setNameValidity(true);
+                }
                 setName(e.target.value);
                 break;
             case "nickName":
@@ -55,11 +68,25 @@ function Registration() {
                 }
                 setPassword(e.target.value);
                 break;
+            case "passwordCheck":
+                setPasswordCheck(e.target.value);
+                break;
         }
     }
+
+    /* 회원가입의 모든 boolean값이 true인지 검증 */
+    const finalValidationCheck = () => {
+        return isValidEmail && isNameFillt && isValidNickname && isValidPassword && isValidPasswordCheck;
+    }
+
+
     /* e는 event를 뜻하고, 대상은 e.target.'대상'으로 선택 */
     const submitClick = (e) => {
-        // e.preventDefault(); react-hook-form 사용 시 필요없으므로 주석처리
+        e.preventDefault();
+        if (!finalValidationCheck()) {
+            alert("검증 안됨");
+            return;
+        }
 
         /* 훅 데이터를 묶어서 전송할 객체 */
         const formData = {
@@ -102,7 +129,7 @@ function Registration() {
                     </div>
                 ) : (
                     <div>
-                        <span>잘못된 Email 양식입니다</span>
+                        <span className='inValidForm'>잘못된 Email 양식입니다</span>
                     </div>
                 )) : null}
                 <input id="name" className="registration_input" name="name" placeholder="성명" onChange={handleFromChange} />
@@ -113,7 +140,7 @@ function Registration() {
                     </div>
                 ) : (
                     <div>
-                        <span>닉네임은 1자 이상 8자 이하로 입력하셔야 합니다</span>
+                        <span className='inValidForm'>닉네임은 1자 이상 8자 이하로 입력하셔야 합니다</span>
                     </div>
                 )) : null}
                 <input className="registration_input" name="password" type="password" placeholder="비밀번호" onChange={handleFromChange} />
@@ -123,17 +150,17 @@ function Registration() {
                     </div>
                 ) : (
                     <div>
-                        <span> 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요</span>
+                        <span className='inValidForm'> 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요</span>
                     </div>
                 )) : null}
-                <input className="registration_input" name="passwordCheck" type="password" placeholder="비밀번호 확인" onChange={handleCheckPassword} />
+                <input className="registration_input" name="passwordCheck" type="password" placeholder="비밀번호 확인" onChange={handleFromChange} />
                 {passwordCheck !== -1 ? (isValidPasswordCheck ? (
                     <div>
                         <span>비밀번호가 일치합니다</span>
                     </div>
                 ) : (
                     <div>
-                        <span>비밀번호가 일치하지 않습니다</span>
+                        <span className='inValidForm'>비밀번호가 일치하지 않습니다</span>
                     </div>
                 )) : null}
                 <div>
