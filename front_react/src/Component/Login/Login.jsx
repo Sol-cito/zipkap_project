81c8ea51e11ca8374435be64a4b9a946ada91c4a
dataenggu <dsolkim1030@gmail.com>
2021-05-05 20:36:19 +0900

    Add react-cookie for login logic

--- front_react/src/Component/Login/Login.jsx
+++ front_react/src/Component/Login/Login.jsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import loginRequestAxios from './LoginRequestAxios'
 import { Redirect } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
 import '../../CSS/Login.css'
 
 function Login() {
@@ -17,6 +18,9 @@ function Login() {
   /* input ref 변수 선언 */
   const idRef = useRef(null);
   const passwordRef = useRef(null);
+
+  /* 로그인 성공 시 설정할 react-cookie */
+  const [cookies, setCookie, removeCookie] = useCookies(['loginDone']);
 
   const handleOnChange = (e) => {
     if (e.target.name == "id") {
@@ -38,6 +42,7 @@ function Login() {
     }
     return true;
   }
+  console.log("setCookie 세팅 전 " + cookies.isLogin);
 
   const handleOnSubmit = (e) => {
     e.preventDefault(); // 리다이렉트 막음
@@ -56,12 +61,18 @@ function Login() {
     loginRequestAxios(formData, (isLoginSuccess) => {
       console.log("로그인 콜백 결과 : " + isLoginSuccess);
       setRedirect(isLoginSuccess); // 콜백결과(true)로 redirect를 세팅한다 -> setState
+
+      /* 로그인이 성공하였을 경우 전역 쿠키(react-cookie)값을 true로 설정한다. */
+      if (isLoginSuccess) {
+        setCookie('loginDone', true, { path: '/' }); // path를 '/'로 하면 전역 쿠키로 기능함
+      }
       setLoginInfoValidation(isLoginSuccess);
     });
   }
   /* 로그인 완료 시 Home화면으로 redirect */
   if (shouldRedirect) {
-    return <Redirect to="/" />
+    // return <Redirect to="/" />
+    window.location.replace("/") // 로그인 완료 시 첫 페이지로 새로고침한다
   }
   return (
     <div className="login_div">
