f57d5d96fc4b2ffd07162d476b5ac423875a2abc
CORP\82692 <kibeom16085@lgcns.com>
2021-06-10 16:51:54 +0900

    map

---
+++ front_react/src/Component/Detail/BarChart.jsx
@@ -0,0 +1,56 @@
+import { Box } from "@material-ui/core";
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+const data = [
+  {
+    name: "동네",
+    매매가: 4000,
+    매매량: 2400,
+  },
+  {
+    name: "한남더힐",
+    매매가: 3000,
+    매매량: 1398,
+  },
+];
+
+export default class Example extends PureComponent {
+  static demoUrl = "https://codesandbox.io/s/simple-bar-chart-tpz8r";
+
+  render() {
+    return (
+      <Box height={400}>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="매매가" fill="#8884d8" />
+            <Bar dataKey="매매량" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
+      </Box>
+    );
+  }
+}
