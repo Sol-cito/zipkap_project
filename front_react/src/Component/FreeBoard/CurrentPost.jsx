2ba95cd77895a95c5f3eb6fcfb89d26d157686b6
dataenggu <dsolkim1030@gmail.com>
2021-06-13 16:43:22 +0900

    Add react-loading and apply to post

--- front_react/src/Component/FreeBoard/CurrentPost.jsx
+++ front_react/src/Component/FreeBoard/CurrentPost.jsx
@@ -3,6 +3,7 @@ import "../../CSS/FreeBoard.css";
 import { useCookies } from 'react-cookie';
 import CurrentPostRequestAxios from './CurrentPostRequestAxios';
 import { convertNumberIntoDateFormatWithDetail } from './convertNumberIntoDateFormat.js'
+import ReactLoading from 'react-loading';
 
 
 const CurrentPost = ({ match }) => {
@@ -17,23 +18,38 @@ const CurrentPost = ({ match }) => {
                 setPost(response);
             }
         }));
-    }, []);
+    }, [post]);
 
     console.log("데이터 받았다 : " + post);
 
     return (
         <div className="mypage_div">
-            <div className="freeBoard_Table_div">
-                {post !== null ? (
-                    <div>
-                        <p>제목 : {post.title}</p>
-                        <p>글쓴이 : {post.author}</p>
-                        <p>날짜 : {convertNumberIntoDateFormatWithDetail(post.date)}</p>
+            <div className="freeboard_curPost_div">
+                {post === null ? (
+                    <div className="react-spinner_div">
+                        <ReactLoading type={'spin'} color={'grey'} height={'50px'} width={'50px'} />
+                    </div>
+                ) : (
+                    <div className="freeboard_content_div">
+                        <h2>{post.title}</h2>
+                        <div className="freeboard_post_info">
+                            <span>{post.author} | </span>
+                            <span>작성 : {convertNumberIntoDateFormatWithDetail(post.date)}</span>
+                        </div>
                         <div dangerouslySetInnerHTML={{ __html: post.content }}></div>
                     </div>
-                ) : null}
+                )}
+                <div>
+                    수정, 추천, 비추천 등
+                </div>
+            </div >
+            <div>
+                댓글
             </div>
-        </div>
+            <div>
+                목록보기
+            </div>
+        </div >
     )
 }
 
